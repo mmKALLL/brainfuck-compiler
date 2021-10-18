56d48d333987d2474274e8296f4e57796f3a66ba
Esa Koskinen <esakoskin2@gmail.com>
2021-10-18 17:10:00 +0900

    first version of the compiler itself, supports .+- with 32 kiB memory now

--- src/brainfuck-compiler.cr
+++ src/brainfuck-compiler.cr
@@ -1,10 +1,22 @@
-# TODO: Write documentation for `Brainfuck`
 module Brainfuck
   VERSION = "0.1.0"
   extend self # Append functions to Brainfuck.self, so they can be called from outside the module.
 
   # Simple parsing of brainfuck. Output is done using comma-separated numeric values, differing from standard Brainfuck spec.
-  def parse(input)
-    input + ' '
+  def parse(input : String) : String
+    position = 16384
+    values = Array.new(32768, 0_u8)
+    output = ""
+    input.each_char do |char|
+      case char
+      when '.'
+        output += values[position].to_s + ","
+      when '+'
+        values[position] += 1
+      when '-'
+        values[position] -= 1
+      end
+    end
+    output
   end
 end
